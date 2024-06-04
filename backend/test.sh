a7745f89eb06007e40806d7bd924eee57adf6f10
moaoh <k010103@naver.com>
2024-06-04 20:00:51 +0900

    feat: :sparkles: nginx 및 spring boot docker container 추가
    docker compose up --build

---
+++ backend/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# target 디렉토리 비우기
+rm -rf target/*
+
+# Gradle을 사용하여 Spring Boot 애플리케이션을 빌드합니다.
+echo "Building Spring Boot application with Gradle..."
+cd ./src/ghub/
+./gradlew clean build
+cd -
+
+# 생성된 JAR 파일을 target 폴더로 이동시킵니다.
+echo "Moving JAR file to target folder..."
+mv src/ghub/build/libs/ghub-0.0.1-SNAPSHOT.jar target/ghub.jar
